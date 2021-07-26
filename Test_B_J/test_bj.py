7f43f7ea00b92e3552ee177929f2866a048f1dff
Nikola1992-prog <85487875+Nikola1992-prog@users.noreply.github.com>
2021-07-26 21:59:57 +0200

    add unittest to project

---
+++ Test_B_J/test_bj.py
@@ -0,0 +1,54 @@
+import unittest
+from BlackJack import Card
+from Players import Player
+from utils.dealer import sum_of_card, win_lose_pass_before_stand, win_lose_pass_after_stand
+
+
+class TestDealer(unittest.TestCase):
+    player = Player('Player_test', 100)
+    coin_value = 20
+
+    player_cards_hand = [[Card('Spades', 'Queen'), Card('Hearts', 'Ace')],  # False BJ
+                         [Card('Hearts', 'Three'), Card('Clubs', 'Ace')],  # True <21
+                         [Card('Hearts', 'King'), Card('Clubs', 'King')],  # True <21
+                         [Card('Hearts', 'Ace'), Card('Clubs', 'Ace')],  # True <21
+                         [Card('Hearts', 'King'), Card('Hearts', 'Queen'), Card('Spades', 'Queen')],  # False >21
+                         [Card('Hearts', 'Three'), Card('Clubs', 'Three')]]  # Flase Player win in after, true before
+
+    dealer_cards_hand = [[Card('Spades', 'Queen'), Card('Hearts', 'Ace')],  # True PASS 21==21
+                         [Card('Hearts', 'King'), Card('Clubs', 'Ace')],  # False 14 < 21 dealer win
+                         [Card('Hearts', 'King'), Card('Clubs', 'Three')],  # False 20 > 13 dealer lose
+                         [Card('Hearts', 'King'), Card('Clubs', 'Five')],  # False dealer win
+                         [Card('Hearts', 'King'), Card('Hearts', 'Three')],  # False, Dealer win
+                         [Card('Hearts', 'Jack'), Card('Hearts', 'Queen'),
+                          Card('Spades', 'Queen')]]  # False dealer lose
+
+    all_cases = player_cards_hand + dealer_cards_hand
+
+    def test_sum_of_card(self):
+        print('Usao1')
+        result_all_cases = [21, 14, 20, 12, 30, 6, 21, 21, 13, 15, 13, 30]
+
+        for index, player_cards in enumerate(self.all_cases):
+            result = sum_of_card(player_cards)
+            self.assertEqual(result, result_all_cases[index])
+
+    def test_win_lose_before_stand(self):
+        print('Usao2')
+        result_win_lose_before_stand = [False, True, True, True, False, True]  # BJ, <21, <21, <21, >21, <21
+        for index, (player_hand, dealer_hand) in enumerate(zip(self.player_cards_hand, self.dealer_cards_hand)):
+            print(index)
+            result = win_lose_pass_before_stand(self.player, dealer_hand, player_hand, self.coin_value)
+            self.assertEqual(result, result_win_lose_before_stand[index])
+
+    def test_win_lose_pass_after_stand(self):
+        print('Usao3')
+        result_win_lose_after_stand = [True, False, True, False, True, False]
+        for index, (player_hand, dealer_hand) in enumerate(zip(self.player_cards_hand, self.dealer_cards_hand)):
+            print(index)
+            result = win_lose_pass_after_stand(self.player, dealer_hand, player_hand, self.coin_value)
+            self.assertEqual(result, result_win_lose_after_stand[index])
+
+
+if __name__ == "__main__":
+    unittest.main()
